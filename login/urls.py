f9547c6af487f42239ad735855edb754b35e4399
Fatema Mahabub <fmahabub8@gmail.com>
2024-05-15 15:54:09 +0000

    add and design the pages

--- login/urls.py
+++ login/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView
+from .views import HomeView, ArticleView
 urlpatterns = [
     # path('login/', views.user_logins, name='login'),  # Changed from views.login to views.user_logins
     # path('home/', views.user_home, name='home'),
     # path('contact/', views.user_contact, name='contact'),
     path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleView.as_view(), name= "article"),
 ]
--- login/views.py
+++ login/views.py
@@ -5,15 +5,10 @@ from django.views.generic import ListView, DetailView  # Corrected import path
 from .models import Post
 
 
-# def user_logins(request):
-#     return render(request, 'authenticate/login.html', {})
-
-# def user_home(request):
-#     return render(request, 'authenticate/home.html', {})
-
-# def user_contact(request):
-#     return render(request, 'authenticate/contact.html', {}) 
-
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+class ArticleView(DetailView):
+     model = Post
+     template_name = 'article.html'
