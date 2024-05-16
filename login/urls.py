6e517e1d8b7228921b792207901722717a324a9f
Fatema Mahabub <fmahabub8@gmail.com>
2024-05-16 17:07:34 +0000

    add the edit option

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, ArticleView
+from .views import HomeView, ArticleView, AddPostView, UpdatePostView
 urlpatterns = [
     # path('login/', views.user_logins, name='login'),  # Changed from views.login to views.user_logins
     # path('home/', views.user_home, name='home'),
     # path('contact/', views.user_contact, name='contact'),
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleView.as_view(), name= "article"),
+    path('add_post/', AddPostView.as_view(), name= "add_post"),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name= "update_post" ),
 ]
--- login/views.py
+++ login/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib import messages
-from django.views.generic import ListView, DetailView  # Corrected import path
+from django.views.generic import ListView, DetailView , CreateView, UpdateView
 from .models import Post
+from .forms import PostForm, EditForm
 
 
 class HomeView(ListView):
@@ -12,3 +13,16 @@ class HomeView(ListView):
 class ArticleView(DetailView):
      model = Post
      template_name = 'article.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    #fields = '__all__'
+    #fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    #fields = ('title', 'title_tag', 'body')
