b1f665653f68e8c878cc5756d7f7fe9615ed1c05
Fatema Mahabub <fmahabub8@gmail.com>
2024-05-20 15:55:52 +0000

    V

--- login/urls.py
+++ login/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-# from . import views
-from .views import HomeView, ArticleView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, ArticleView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
+
 urlpatterns = [
-    # path('login/', views.user_logins, name='login'),  # Changed from views.login to views.user_logins
-    # path('home/', views.user_home, name='home'),
-    # path('contact/', views.user_contact, name='contact'),
     path('', HomeView.as_view(), name="home"),
-    path('article/<int:pk>', ArticleView.as_view(), name= "article"),
-    path('add_post/', AddPostView.as_view(), name= "add_post"),
-    path('article/edit/<int:pk>', UpdatePostView.as_view(), name= "update_post" ),
-    path('article/<int:pk>/remove', DeletePostView.as_view(), name= "delete_post" ),
+    path('article/<int:pk>', ArticleView.as_view(), name="article"),
+    path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
     path('add_category/', AddCategoryView.as_view(), name="add_category"),
-    path('category/<str:cats>/', CategoryView, name= 'category'),
+    path('category/<str:cats>/', CategoryView, name='category'),
+    path('category-list/', CategoryListView, name='category-list'),
 ]
